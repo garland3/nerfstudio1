5441983fe558f037f2cdc642e026c85b4f895ed3
Anthony Garland <garland3@gmail.com>
2022-10-14 08:00:04 -0600

    Update installeverything.sh

--- installeverything.sh
+++ installeverything.sh
@@ -1,24 +1,24 @@
-cd /workspace
+cd /home/root
 mkdir temp
 mkdir anaconda
 mkdir git
 cd temp
 wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.12.0-Linux-x86_64.sh
 chmod +x Miniconda3-py38_4.12.0-Linux-x86_64.sh
-./Miniconda3-py38_4.12.0-Linux-x86_64.sh -b -f -p /workspace/anaconda
+./Miniconda3-py38_4.12.0-Linux-x86_64.sh -b -f -p /home/root/anaconda
 # I had to move to the anaconda location to find conda
-cd /workspace/anaconda/bin
-./conda init bash
-/workspace/anaconda/bin
+# cd /home/root/anaconda/bin
+# ./conda init bash
+
 
 # https://askubuntu.com/a/988191
-export PATH="/workspace/anaconda/bin:$PATH"
+export PATH="/home/root/anaconda/bin:$PATH"
 
 
  # LOG OUT AND BACK IN
 
 apt-get update
-apt-get install build-essential git  libgl1-mesa-glx libglib2.0-0   -y
+apt-get install build-essential git  libgl1-mesa-glx libglib2.0-0 cmake  -y
 # apt-get install  nvidia-cuda-toolkit -y
 
 export PATH="/usr/local/cuda-11.3/bin:$PATH"
@@ -28,7 +28,13 @@ export LD_LIBRARY_PATH="/usr/local/cuda-11.3/lib64:$LD_LIBRARY_PATH"
 # ./conda activate nerfstudio
 python -m pip install --upgrade pip
 pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 -f https://download.pytorch.org/whl/torch_stable.html
-pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+
+cd /home/root/temp
+git clone https://github.com/NVlabs/tiny-cuda-nn
+cd tiny-cuda-nn
+cmake . -B build
+cmake --build build --config RelWithDebInfo -j
+# pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 
 
 pip install nerfstudio
